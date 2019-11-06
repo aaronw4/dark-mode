a140efce55748b153c7b77d2f546e6da6d4c1950
Aaron Williams <aaronrw4@yahoo.com>
2019-11-05 22:18:24 -0800

    Finished MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,16 +1,16 @@
-import React, {useEffect} from 'react';
-import useLocalStorage from './useLocalStorage';
+import {useEffect} from 'react';
+import {useLocalStorage} from './useLocalStorage';
 
 export const useDarkMode = () => {
     const [value, setValue] = useLocalStorage('dark');
 
     useEffect(() => {
-        if (useLocalStorage === true) {
+        if (value === true) {
             document.querySelector('body').classList.add("dark-mode");
         } else {   
             document.querySelector('body').classList.remove('dark-mode');
         }
-    }[value])
+    },[value])
 
     return([value, setValue])
 }
